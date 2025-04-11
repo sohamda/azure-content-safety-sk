48f8d07da3a94e8a573fde36f5921cffa6a71dca
sohamda <iam.soham@gmail.com>
2025-04-11 12:19:12 +0200

    added promptshield check

---
+++ src/main/java/soham/content/safety/api/client/PromptShieldChecker.java
@@ -0,0 +1,46 @@
+package soham.content.safety.api.client;
+
+import com.apisdk.ApiClient;
+import com.apisdk.models.ShieldPromptOptions;
+import com.apisdk.models.ShieldPromptResult;
+import com.microsoft.kiota.authentication.AnonymousAuthenticationProvider;
+import com.microsoft.kiota.bundle.DefaultRequestAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import static soham.content.safety.util.PropertyReader.getProperty;
+
+public class PromptShieldChecker {
+
+    private static final Logger log = LoggerFactory.getLogger(PromptShieldChecker.class);
+
+    public static String CONTENT_SAFETY_ENDPOINT = "client.azure.content.safety.endpoint";
+    public static String CONTENT_SAFETY_KEY = "client.azure.content.safety.key";
+
+    public boolean isPromptSafe(String prompt) throws IOException {
+
+        ShieldPromptOptions shieldPromptOptions = new ShieldPromptOptions();
+        shieldPromptOptions.setUserPrompt(prompt);
+
+        final String key = getProperty(CONTENT_SAFETY_KEY);
+        final String endpoint = getProperty(CONTENT_SAFETY_ENDPOINT);
+
+        final AnonymousAuthenticationProvider authProvider =
+                new AnonymousAuthenticationProvider();
+        final DefaultRequestAdapter adapter = new DefaultRequestAdapter(authProvider);
+        adapter.setBaseUrl(endpoint);
+        ApiClient apiClient = new ApiClient(adapter);
+
+        ShieldPromptResult post = apiClient.textShieldPrompt().withUrl(endpoint + "/text:shieldPrompt?api-version=2024-09-15-preview").post(shieldPromptOptions,
+                requestConfiguration -> {
+                    requestConfiguration.headers.put("Ocp-Apim-Subscription-Key", Collections.singleton(key));
+                }
+        );
+        Boolean attackDetected = post.getUserPromptAnalysis().getAttackDetected();
+        log.debug("Is prompt shield checked: {}", attackDetected);
+        return attackDetected;
+    }
+}
